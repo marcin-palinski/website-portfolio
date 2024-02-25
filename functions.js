7384d5d970324e114a0cc6d31a1232627e867adb
Marcin <marcinpalinski@wp.pl>
2024-02-25 13:55:36 +0100

    version with hamburger menu

--- functions.js
+++ functions.js
@@ -1,8 +1,21 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
-const menu = document.querySelector(".menu-box");
-const logo = document.querySelector(".logo");
-
-logo.addEventListener('click', () => {
-    menu.classList.add("show");
-    console.log("click");
+const bigMenu = document.querySelector(".menu-box");
+const bar1 = document.querySelector("#one");
+const bar2 = document.querySelector("#two");
+const bar3 = document.querySelector("#three");
+hamburgerMenu.addEventListener("click", () => {
+  bigMenu.classList.toggle("visible");
+  bar1.classList.toggle("one");
+  bar2.classList.toggle("two");
+  bar3.classList.toggle("three");
+  })
+hamburgerMenu.addEventListener("mouseover", () => {
+  const bar = document.querySelectorAll(".hamburger-bar");
+  for (let bars of bar) {
+  bars.classList.add("color");}
+})
+hamburgerMenu.addEventListener("mouseout", () => {
+  const bar = document.querySelectorAll(".hamburger-bar");
+  for (let bars of bar) {
+  bars.classList.remove("color");}
 })
