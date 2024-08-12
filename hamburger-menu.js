b2b1474690ab2a5a674d105a8dd73368f36035a4
Marcin <marcinpalinski@wp.pl>
2024-08-13 00:27:38 +0200

    after all changes

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-// const hamburgerMenu = document.querySelector(".hamburger-menu");
-// const bigMenu = document.querySelector(".menu-box");
-// const bar1 = document.querySelector("#one");
-// const bar2 = document.querySelector("#two");
-// const bar3 = document.querySelector("#three");
-// hamburgerMenu.addEventListener("click", () => {
-//   bigMenu.classList.toggle("visible");
-//   bar1.classList.toggle("one");
-//   bar2.classList.toggle("two");
-//   bar3.classList.toggle("three");
-//   })
-// hamburgerMenu.addEventListener("mouseover", () => {
-//   const bar = document.querySelectorAll(".hamburger-bar");
-//   for (let bars of bar) {
-//   bars.classList.add("color");}
-// })
-// hamburgerMenu.addEventListener("mouseout", () => {
-//   const bar = document.querySelectorAll(".hamburger-bar");
-//   for (let bars of bar) {
-//   bars.classList.remove("color");}
-// })
-=======
-=======
->>>>>>> 8e702ae7923b78016e6e0355f2d36b63ce967b36
 // hamburger-menu
 
 const hamburgerMenu = document.querySelector(".hamburger-menu");
@@ -46,9 +20,4 @@ hamburgerMenu.addEventListener("mouseout", () => {
   const bar = document.querySelectorAll(".hamburger-bar");
   for (let bars of bar) {
   bars.classList.remove("color");}
-<<<<<<< HEAD
 })
->>>>>>> 0acaac1 (changed slides of tools)
-=======
-})
->>>>>>> 8e702ae7923b78016e6e0355f2d36b63ce967b36
