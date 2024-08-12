932b61f45068e8990ae1c7eb7b55b0e775c23eeb
Marcin <marcinpalinski@wp.pl>
2024-08-13 00:05:31 +0200

    added new files

---
+++ hamburger-menu.js
@@ -0,0 +1,47 @@
+<<<<<<< HEAD
+// const hamburgerMenu = document.querySelector(".hamburger-menu");
+// const bigMenu = document.querySelector(".menu-box");
+// const bar1 = document.querySelector("#one");
+// const bar2 = document.querySelector("#two");
+// const bar3 = document.querySelector("#three");
+// hamburgerMenu.addEventListener("click", () => {
+//   bigMenu.classList.toggle("visible");
+//   bar1.classList.toggle("one");
+//   bar2.classList.toggle("two");
+//   bar3.classList.toggle("three");
+//   })
+// hamburgerMenu.addEventListener("mouseover", () => {
+//   const bar = document.querySelectorAll(".hamburger-bar");
+//   for (let bars of bar) {
+//   bars.classList.add("color");}
+// })
+// hamburgerMenu.addEventListener("mouseout", () => {
+//   const bar = document.querySelectorAll(".hamburger-bar");
+//   for (let bars of bar) {
+//   bars.classList.remove("color");}
+// })
+=======
+// hamburger-menu
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const bigMenu = document.querySelector(".menu-box");
+const bar1 = document.querySelector("#one");
+const bar2 = document.querySelector("#two");
+const bar3 = document.querySelector("#three");
+hamburgerMenu.addEventListener("click", () => {
+  bigMenu.classList.toggle("visible");
+  bar1.classList.toggle("one");
+  bar2.classList.toggle("two");
+  bar3.classList.toggle("three");
+  })
+hamburgerMenu.addEventListener("mouseover", () => {
+  const bar = document.querySelectorAll(".hamburger-bar");
+  for (let bars of bar) {
+  bars.classList.add("color");}
+})
+hamburgerMenu.addEventListener("mouseout", () => {
+  const bar = document.querySelectorAll(".hamburger-bar");
+  for (let bars of bar) {
+  bars.classList.remove("color");}
+})
+>>>>>>> 0acaac1 (changed slides of tools)
